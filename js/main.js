8b97aa70bb704a5a30f996eeb973232624f1291a
rubo-21 <rubenyeghikyan@gmail.com>
2015-07-16 03:28:29 +0400

    added animations

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var svg = Snap("#svg");
+var current = 0;
+var words = [
+	"Don't touch me!",
+	"Hey!",
+	"Are you crazy?!",
+	"Rrrrrr..."
+];
+
+var Circle = svg.circle(84, 76, 72);
+
+Circle.attr({
+	fill: "none",
+	strokeWidth: 5
+});
+
+/*
+Circle.hover(function (event) {
+	this.attr({stroke: "#6FDA44"}, 200, mina.easeout);
+});*/
+
+$('#svg').mouseover(function(event) {
+	//Circle.animate({stroke: "#f00"}, 50);
+	Circle.attr({stroke: "#6FDA44"});
+});
+
+$('#svg').mouseout(function(event) {
+	//Circle.animate({stroke: "none"}, 50);
+	Circle.attr({stroke: "none"});
+});
+
+$('#svg').click(function(event) {
+	if(current < words.length && words[current]){
+		$('#messageBox').text(words[current]).fadeIn();
+		current++;
+	}
+	else {
+		$('.personBlock').effect("explode", "slow");
+		$('.hire').effect("explode", "slow");
+		$('#nothing').fadeIn('slow');
+	}
+});
